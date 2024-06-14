2c49bca4adc3fa2ee6fc1fce4f59b9f912d5d830
Saeed Badran <salouri@gmail.com>
2024-06-14 22:34:44 +0300

    modified VALUE_ENCODING

--- src/config.js
+++ src/config.js
@@ -1,2 +1,20 @@
 export const COMMON_TOPIC = Buffer.alloc(32).fill('decentrapay');
-export const VALUE_ENCODING = {valueEncoding: 'json'};
+export const VALUE_ENCODING = {
+  preencode(state, value) {
+    if (typeof value === 'string') {
+      state.end += Buffer.byteLength(value);
+    } else {
+      throw new TypeError('Expected value to be a string');
+    }
+  },
+  encode(state, value) {
+    if (typeof value === 'string') {
+      state.buffer.write(value);
+    } else {
+      throw new TypeError('Expected value to be a string');
+    }
+  },
+  decode(state) {
+    return state.buffer.toString();
+  },
+};

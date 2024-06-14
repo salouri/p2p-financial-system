f0cc362ead6766f98091b9244a291e7cb8fca7fb
Saeed Badran <salouri@gmail.com>
2024-06-14 22:36:26 +0300

    extracted client.on(open) out of swarm.on('connection')

--- src/peer-setup.js
+++ src/peer-setup.js
@@ -1,7 +1,7 @@
 import RPC from '@hyperswarm/rpc';
 import DHT from 'hyperdht';
 import Hyperswarm from 'hyperswarm';
-import {COMMON_TOPIC} from './config.js';
+import {COMMON_TOPIC, VALUE_ENCODING} from './config.js';
 import {registerRpcEvents} from './utils.js';
 
 export async function startPeer(storageDir, initialServerPublicKey) {
@@ -21,53 +21,55 @@ export async function startPeer(storageDir, initialServerPublicKey) {
     await commonDiscovery.flushed();
     publicKeyBuffer = rpc.defaultKeyPair.publicKey;
   }
-  console.log('>> publicKeyBuffer: ', publicKeyBuffer);
 
   const client = rpc.connect(publicKeyBuffer);
+
+  client.on('open', async () => {
+    console.log('Client RPC connection opened');
+
+    if (!initialServerPublicKey) {
+      try {
+        const {publicKey} = await client.request(
+          'sendPublicKey',
+          {},
+          {valueEncoding: VALUE_ENCODING},
+        );
+        publicKeyBuffer = Buffer.from(publicKey, 'hex');
+        console.log(
+          'Received server public key:',
+          publicKeyBuffer.toString('hex'),
+        );
+      } catch (error) {
+        console.error('Error receiving server public key:', error);
+      }
+    }
+
+    const sendArgs = {sender: 'Alice', receiver: 'Bob', amount: 100};
+    try {
+      const sendResponse = await client.request('sendTransaction', [sendArgs], {
+        valueEncoding: VALUE_ENCODING,
+      });
+      console.log('Transaction sent:', sendResponse);
+    } catch (error) {
+      console.error('Error sending transaction:', error);
+    }
+
+    try {
+      const getResponse = await client.request('getTransaction', [{index: 0}], {
+        valueEncoding: VALUE_ENCODING,
+      });
+      console.log('Transaction received:', getResponse);
+    } catch (error) {
+      console.error('Error getting transaction:', error);
+    }
+  });
 
   swarm.on('connection', (socket, peerInfo) => {
     console.log('Peer connected to the server');
     registerRpcEvents(client);
-
-    client.on('open', async () => {
-      console.log('Client RPC connection opened');
-
-      if (!initialServerPublicKey) {
-        try {
-          const {publicKey} = await client.request('sendPublicKey', {});
-          publicKeyBuffer = Buffer.from(publicKey, 'hex');
-          console.log(
-            'Received server public key:',
-            publicKeyBuffer.toString('hex'),
-          );
-        } catch (error) {
-          console.error('Error receiving server public key:', error);
-        }
-      }
-
-      const sendArgs = {sender: 'Alice', receiver: 'Bob', amount: 100};
-      try {
-        const sendResponse = await client.request('sendTransaction', [
-          sendArgs,
-        ]);
-        console.log('Transaction sent:', sendResponse);
-      } catch (error) {
-        console.error('Error sending transaction:', error);
-      }
-
-      try {
-        const getResponse = await client.request('getTransaction', [
-          {index: 0},
-        ]);
-        console.log('Transaction received:', getResponse);
-      } catch (error) {
-        console.error('Error getting transaction:', error);
-      }
-    });
   });
 
   await swarm.flush();
   console.log('Client is ready and looking for servers');
-
-  console.log('Peer is running...');
+  console.log('Peer is running');
 }

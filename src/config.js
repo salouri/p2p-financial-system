fd63c92bc98497f947d6d2e4bf65e147b67ccd45
Saeed Badran <salouri@gmail.com>
2024-06-15 01:24:25 +0300

    comments

--- src/config.js
+++ src/config.js
@@ -1,6 +1,15 @@
 export const COMMON_TOPIC = Buffer.alloc(32).fill('decentrapay');
+/**
+ * Provides encoding and decoding utilities for string values.
+ *
+ * The `preencode` function calculates the expected length of the encoded value.
+ * The `encode` function writes the string value to the provided buffer.
+ * The `decode` function reads the string value from the provided buffer.
+ */
 export const VALUE_ENCODING = {
   preencode(state, value) {
+    // state is a reference to the current encoding state: {buffer, end}
+    // where end is the offset of the next byte to write to the buffer
     if (typeof value === 'string') {
       state.end += Buffer.byteLength(value);
     } else {

2bfb0523fd6a0dd80d694ef84a2f533a6336fd5d
Saeed Badran <salouri@gmail.com>
2024-06-14 04:47:35 +0300

    accept parameters for connections

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-import {startClient} from './src/hyperswarm-setup.js';
+import startNode from './src/hyperswarm-setup.js';
 
-const publicKeyStr =
-  '25cd218aa54923c581eabb9be677b35962870b24c990f94a1d987962ca47bae2';
-startClient(publicKeyStr);
+/*
+  mode: Passed as 'peer' when starting the application
+  peerId: Unique identifier for each peer
+  serverPublicKey - Optional: Public key of the server for initial connection
+*/
+const [, , mode, peerId, serverPublicKey] = process.argv;
+if (!peerId) {
+  console.error('Error: You must provide a unique peer identifier.');
+  process.exit(1);
+}
+
+const storageDir = `./transactions-core-${peerId}`;
+
+startNode(mode, storageDir, serverPublicKey);

e9d1fee5302adc463ab2991d802c818d2741f7f5
Saeed Badran <salouri@gmail.com>
2024-06-16 01:49:39 +0300

    use same publicKey for server

--- src/server-setup.js
+++ src/server-setup.js
@@ -6,49 +6,69 @@ import { createCoreAndBee, registerRpcEvents } from './utils.js';
 
 dotenv.config();
 export async function startServer(storageDir) {
-  const {core, db} = await createCoreAndBee(storageDir, VALUE_ENCODING);
-
+  const keyPair = {
+    publicKey: Buffer.from(process.env.PUBLIC_KEY, 'hex'),
+    secretKey: Buffer.from(process.env.SECRET_KEY, 'hex'),
+  };
   const dht = new DHT();
-  // Initialize the RPC server with the DHT instance
-  const rpc = new RPC({dht});
+  await dht.ready();
+  const rpc = new RPC({dht, keyPair});
 
   const server = rpc.createServer();
+  const {core, hbee} = await createCoreAndBee(storageDir, VALUE_ENCODING);
+
+  const serverPublicKey = rpc.defaultKeyPair.publicKey.toString('hex');
 
   // Register the methods for handling requests
   server.respond('sendPublicKey', async () => {
     console.log('Received request for sending public key');
-    return {publicKey: rpc.defaultKeyPair.publicKey.toString('hex')};
+    return {publicKey: serverPublicKey};
   });
 
   server.respond('sendTransaction', async req => {
-    const data = JSON.parse(req.toString());
-    console.log('Received transaction details:', data);
-    const {sender, receiver, amount} = data;
-    const transaction = {sender, receiver, amount, timeStamp: Date.now()};
-    const logRecord = JSON.stringify({type: 'transaction', value: transaction});
-    await core.append(logRecord);
-    return Buffer.from(JSON.stringify({status: true, transaction}));
+    try {
+      const data = JSON.parse(req.toString());
+      console.log('Received transaction-data:', data);
+      const {sender, receiver, amount} = data;
+      const transaction = {sender, receiver, amount, timeStamp: Date.now()};
+      const key = `transaction:${transaction.timeStamp}`;
+      await hbee.put(key, transaction);
+      return Buffer.from(JSON.stringify({status: true, key, transaction}));
+    } catch (error) {
+      console.error('Error sending transaction:', error);
+      return Buffer.from(JSON.stringify({status: false, error}));
+    }
   });
 
   server.respond('getTransaction', async req => {
-    console.log(
-      'Received request for transaction:',
-      JSON.parse(req.toString()),
-    );
-    const {index} = JSON.parse(req.toString());
-    const data = await core.get(index);
-    return Buffer.from(JSON.stringify(data));
+    try {
+      console.log(
+        'Received request for transaction:',
+        JSON.parse(req.toString()),
+      );
+      const {key} = JSON.parse(req.toString());
+      console.log('key: ', key);
+      let transaction;
+      try {
+        transaction = await hbee.get(key);
+      } catch (error) {
+        console.error(error);
+        throw new Error('Error getting transaction!');
+      }
+      console.log('transaction: ', transaction);
+      const data = transaction.value;
+      console.log('data: ', data);
+      return Buffer.from(JSON.stringify(data));
+    } catch (error) {
+      console.error('Error getting transaction:', error);
+      return Buffer.from(JSON.stringify({status: false, error}));
+    }
   });
 
   await server.listen();
   console.log('Server is listening...');
 
-  const serverPublicKey = rpc.defaultKeyPair.publicKey.toString('hex');
   console.log('Server public key:', serverPublicKey);
-  // check if public key is 32 bytes:
-  if (serverPublicKey.length !== 64) {
-    throw new Error('Server public key is not 32 bytes');
-  }
 
   const topic = Buffer.alloc(32).fill(COMMON_TOPIC);
   dht.announce(topic, rpc.defaultKeyPair, err => {

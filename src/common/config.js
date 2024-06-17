72da93a8d57d6f438a5d560db50197d5015bc53b
Saeed Badran <salouri@gmail.com>
2024-06-17 21:33:38 +0300

    bootstrapNodes

--- src/common/config.js
+++ src/common/config.js
@@ -1,6 +1,12 @@
-export const COMMON_TOPIC = Buffer.alloc(32).fill('decentrapay');
+import dotenv from 'dotenv';
 
+dotenv.config();
+
+export const commonTopic = Buffer.alloc(32).fill('decentrapay');
 export const VALUE_ENCODING = 'json';
-
-export const BOOTSTRAP_NODES = [{host: '127.0.0.1', port: 49737}];
-//  hyperdht --bootstrap --host 127.0.0.1 --port 49737
+export const bootstrapNodes = [{host: '127.0.0.1', port: 30001}];
+export const keyPair = {
+  publicKey: Buffer.from(process.env.PUBLIC_KEY, 'hex'),
+  secretKey: Buffer.from(process.env.SECRET_KEY, 'hex'),
+};
+//  hyperdht --bootstrap --host 127.0.0.1 --port 30001

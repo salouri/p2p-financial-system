86fc5a214921f0b2781ca018a4a6b4cfd30ca2e7
Saeed Badran <salouri@gmail.com>
2024-06-20 00:27:23 +0300

    add logic for extracting bidders public key (coming connection requests)

--- src/peer/getClientPublicKey.js
+++ src/peer/getClientPublicKey.js
@@ -1,17 +1,17 @@
-function getClientPublicKey(client) {
-  if (client?.publicKey) {
-    // console.log('found inside client.publicKey');
-    return client.publicKey.toString('hex');
+function getClientPublicKey(client, nodeType) {
+  if (nodeType === 'bidders' && client?._mux?.stream?.remotePublicKey) {
+    return client?._mux?.stream?.remotePublicKey.toString('hex');
+  } else if (nodeType === 'sellers') {
+    if (client?._publicKey) {
+      return client._publicKey.toString('hex');
+    }
+    if (client?._mux?.stream?.publicKey) {
+      return client._mux.stream.publicKey.toString('hex');
+    }
+    throw new Error('Public key not found');
+  } else {
+    throw new Error('Uknown node type! Types used are "bidders" or "sellers"');
   }
-  if (client?._publicKey) {
-    // console.log('found inside client._publicKey');
-    return client._publicKey.toString('hex');
-  }
-  if (client?._mux?.stream?.publicKey) {
-    // console.log('found inside client._mux.stream.publicKey');
-    return client._mux.stream.publicKey.toString('hex');
-  }
-  throw new Error('Public key not found');
 }
 
 export default getClientPublicKey;

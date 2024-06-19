e0ca9f24a94b2219542e1389f138ede97d5abaf7
Saeed Badran <salouri@gmail.com>
2024-06-20 00:26:14 +0300

    move + add loading functionality

---
+++ src/peer/manageKnownPeers.js
@@ -0,0 +1,23 @@
+// common/utils/manageKnownPeers.js
+import fs from 'fs';
+import path from 'path';
+
+const FILE_NAME = 'known-peers.json';
+export function loadKnownPeers(storageDir, keyPair) {
+  const filePath = path.join(storageDir, FILE_NAME);
+  let data = [];
+  if (fs.existsSync(filePath)) {
+    const peersList = fs.readFileSync(filePath);
+    data = JSON.parse(peersList);
+  }
+  console.log('Done loading known peers.');
+  return data;
+}
+
+export const saveKnownPeers = (storageDir, peers) => {
+  const filePath = path.join(storageDir, FILE_NAME);
+
+  fs.writeFileSync(filePath, JSON.stringify(peers, null, 2));
+};
+
+export default {loadKnownPeers, saveKnownPeers};

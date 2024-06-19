4a45cacda2866653c342e09f4ca45460bc3816c9
Saeed Badran <salouri@gmail.com>
2024-06-20 00:29:23 +0300

    introduce nodeType (bidder/seller) to peer events

--- src/peer/registerPeerEvents.js
+++ src/peer/registerPeerEvents.js
@@ -1,20 +1,26 @@
 // peer/registerPeerEvents.js
 import getClientPublicKey from './getClientPublicKey.js';
-export default function registerPeerEvents(client, connectedPeers) {
-  const publicKey = getClientPublicKey(client);
+export default function registerPeerEvents(client, connectedPeers, nodeType) {
+  const publicKey = getClientPublicKey(client, nodeType);
 
   client.on('open', async () => {
-    console.log('Peer connection opened');
-    connectedPeers.set(publicKey, {client, timestamp: Date.now()});
+    const clientType = nodeType.substring(0, nodeType.length - 1);
+    console.log(`${clientType} peer connection opened`);
+    console.log(
+      `adding ${clientType} client to connectedPeers: ${String(
+        publicKey,
+      ).substring(0, 15)}...`,
+    );
+    connectedPeers[nodeType].set(publicKey, {client, timestamp: Date.now()});
   });
 
   client.on('close', () => {
-    connectedPeers.delete(publicKey);
+    connectedPeers[nodeType].delete(publicKey);
     console.log('Peer connection closed');
   });
 
   client.on('destroy', () => {
-    connectedPeers.delete(publicKey);
+    connectedPeers[nodeType].delete(publicKey);
     console.log('Peer connection destroyed');
   });
 }

51ce807c4f4316e342f65f68ef81b46fb393bb9f
Saeed Badran <salouri@gmail.com>
2024-06-21 02:13:54 +0300

    structure path os-agnostic

--- index.js
+++ index.js
@@ -1,31 +1,19 @@
 import dotenv from 'dotenv';
-import fs from 'fs-extra';
+import path from 'path';
 import {startNode} from './src/node-setup.js';
 import {loadKnownPeers} from './src/peer/manageKnownPeers.js';
 import checkBootstrapNodes from './src/common/utils/checkBootstrapNodes.js';
 
 dotenv.config();
 
-const clearStorage = path => {
-  if (process.env.NODE_ENV === 'development') {
-    try {
-      fs.removeSync(path);
-      console.log('Storage cleared successfully');
-    } catch (err) {
-      console.error('Error clearing storage:', err);
-    }
-  }
-};
 if (await checkBootstrapNodes) {
   console.log('Bootstrap Nodes are running...');
 
   const [, , id, serverPublicKey] = process.argv;
 
-  let storageDir = `./storage/node-${id}`;
+  let storageDir = path.join('.', 'storage', `node-${id}`);
 
-  // clearStorage(storageDir);
-
-  // Load previously connected peers, if any
+  // Load previously connected peers, if any, from a file
   const knownPeers = loadKnownPeers(storageDir);
 
   await startNode(storageDir, serverPublicKey);

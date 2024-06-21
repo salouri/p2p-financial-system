ba05dac60637e4a2700d156d773e789f922e5be5
Saeed Badran <salouri@gmail.com>
2024-06-21 05:09:07 +0300

    use RAM in development

--- src/common/utils/initializeDb.js
+++ src/common/utils/initializeDb.js
@@ -1,23 +1,23 @@
 // common/utils/initializeDb.js
 import Hyperbee from 'hyperbee';
 import Hypercore from 'hypercore';
+import RAM from 'random-access-memory';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
+const inDevelopment = process.env.NODE_ENV === 'development';
 // Function to initialize a Hyperbee instance
-export default async function initializeDb({
-  storageDir,
-  keyPair = null,
-  valueEncoding = 'json',
-}) {
-  if (!keyPair) {
-    keyPair = {
-      publicKey: Buffer.from(process.env.PUBLIC_KEY, 'hex'),
-      secretKey: Buffer.from(process.env.SECRET_KEY, 'hex'),
-    };
-  }
-  const core = new Hypercore(storageDir, keyPair, {valueEncoding});
+const initializeDb = async (storageDir, valueEncoding = 'json') => {
+  // fixed shared keyPair ensures replicated feed among peers
+  const keyPair = {
+    publicKey: Buffer.from(process.env.PUBLIC_KEY, 'hex'),
+    secretKey: Buffer.from(process.env.SECRET_KEY, 'hex'),
+  };
+
+  const storageLoc = inDevelopment ? () => new RAM() : storageDir;
+
+  const core = new Hypercore(storageLoc, keyPair, {valueEncoding});
   await core.ready();
 
   const db = new Hyperbee(core, {
@@ -26,5 +26,7 @@ export default async function initializeDb({
   });
   await db.ready();
 
-  return {core, db};
-}
+  return db;
+};
+
+export default initializeDb;

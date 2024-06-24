7767a7fb6bef1be4d19c67ebcf48e89c2702194a
Saeed Badran <salouri@gmail.com>
2024-06-24 20:32:25 +0300

    close db on shutdown

--- src/common/utils/handleShutdown.js
+++ src/common/utils/handleShutdown.js
@@ -1,27 +1,28 @@
 // common/utils/handleShutdown.js
-import {saveKnownPeers} from '../../network/index.js';
 import getAllPeers from '../../peer/getAllPeers.js';
 import state from '../../common/state/index.js';
 import eventEmitter from '../events/eventEmitter.js';
+import {savePeerConnections} from '../../network/index.js';
 
 export default async function handleShutdown(swarm, storageDir) {
   async function gracefulShutdown() {
-    const allPeers = getAllPeers();
+    const bidderPeers = state.connectedPeers?.bidders.values();
+
     try {
-      await state.db.close();
-    } catch (error) {
-      console.error('Error closing db:', error);
-    }
-    try {
-      if (allPeers) {
+      if (bidderPeers.length) {
         eventEmitter.emit('serverShutdown');
-        saveKnownPeers(storageDir, allPeers);
+        await savePeerConnections(state.db, bidderPeers);
       }
     } catch (error) {
       console.error('Error notifying peers or saving connected ones:', error);
     }
 
-    // on
+    try {
+      await state.db.close();
+    } catch (error) {
+      console.error('Error closing db:', error);
+    }
+
     swarm.once('close', function () {
       process.exit();
     });
@@ -29,18 +30,15 @@ export default async function handleShutdown(swarm, storageDir) {
     swarm.destroy();
 
     // Ensure exit if swarm doesn't close in time
-    setTimeout(() => process.exit(), 1000);
+    setTimeout(() => process.exit(), 3000);
   }
 
   // Handle SIGINT (Ctrl+C)
   process.once('SIGINT', gracefulShutdown);
-
   // Handle SIGTERM (kill command)
   process.once('SIGTERM', gracefulShutdown);
-
   // Handle SIGHUP (terminal closing)
   process.once('SIGHUP', gracefulShutdown);
-
   // Handle SIGUSR1 and SIGUSR2 (user-defined signals)
   process.once('SIGUSR1', gracefulShutdown);
   process.once('SIGUSR2', gracefulShutdown);
